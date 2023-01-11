e89dd579648407dc8baa33229db652bd3963de40
amalhotra40 <abhinav@culturebiosciences.com>
2023-01-11 00:10:46 -0500

    using public repo

--- insilicho/reactor.py
+++
@@ -1,6 +0,0 @@
-import dataclasses
-
-
-@dataclasses.dataclass
-class Vessel:
-    volume: float = 250 / 1000
--- insilicho/units.py
+++
@@ -1,9 +0,0 @@
-import typing
-
-import pint
-
-# Setup the pint unit-tracking system.
-# All units must come from same registry.
-UNIT = pint.UnitRegistry()
-
-UnitType = typing.Any
